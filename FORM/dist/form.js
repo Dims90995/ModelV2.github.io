4d316ad903bab8d6fd8ce7a828890d315b13229c
Dima Shamis <dims90995@gmail.com>
2025-02-09 20:28:37 +0200

    update

---
+++ FORM/dist/form.js
@@ -0,0 +1,6 @@
+function form() {
+    var form = document.createElement('form');
+    form.setAttribute('method', 'post');
+    form.setAttribute('action', 'submit.php');
+    return form;
+}
---
+++ FORM/form.ts
@@ -0,0 +1,7 @@
+function   form() {
+  var form = document.createElement('form');
+  form.setAttribute('method', 'post');
+  form.setAttribute('action', 'submit.php');
+  return form;
+}
+
